a9a4a5df7353d1f3e9ed15e42eb14693e81f0317
Allan <allanfelipedk@gmail.com>
2023-01-22 20:55:08 -0300

    Variables

---
+++ Algorithms/src/Game/Variables.java
@@ -0,0 +1,13 @@
+package Game;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Allan";
+        int age = 19;
+        double weight = 75.78;
+
+        System.out.println("Name -> " + name);
+        System.out.println("Age -> " + age + " years old");
+        System.out.println("Weight -> " + weight);
+    }
+}
