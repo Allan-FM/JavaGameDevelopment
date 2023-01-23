e746f771f00e529d24b00290cee760720063c65d
Allan <allanfelipedk@gmail.com>
2023-01-23 19:43:16 -0300

    repeating loops

---
+++ Algorithms/src/Game/RepeatingLoops.java
@@ -0,0 +1,16 @@
+package Game;
+
+public class RepeatingLoops {
+    public static void main(String[] args) {
+        /*
+        int cout = 0;
+        while (cout <= 10) {
+            System.out.println(cout);
+            cout++;
+        }
+        */
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
