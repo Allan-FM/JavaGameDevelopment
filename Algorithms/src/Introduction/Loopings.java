1addb306549584e545f3e83221eba112af093e3b
Allan <allanfelipedk@gmail.com>
2023-01-25 20:34:27 -0300

    introductory module to algorithms focused on game in java language

---
+++ Algorithms/src/Introduction/Loopings.java
@@ -0,0 +1,23 @@
+package Introduction;
+
+public class Loopings {
+    public static void main(String[] args) {
+        int cout = 0;
+        /*
+         * while (cout <= 10) {
+         * System.out.println(cout);
+         * cout++;
+         * }
+         */
+        /*
+         * for (int i = 0; i <= 10; i++) {
+         * System.out.println(i);
+         * }
+         */
+        do{
+            System.out.println(cout);
+            cout++;
+        }while(cout <= 10);
+
+    }
+}
